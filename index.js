6381f146169a08c3e656eb01f8cd0a4d70d7b593
Mark Brian <markbkiunga2017@gmail.com>
2024-06-26 11:30:18 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.ceil(Math.random());
+console.log(random);
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(20, 15, 11);
+console.log(max);
